8df8c4f66cf4166d9365b190630f5f39d610f0b3
Manish Motiani <manikmanshu@gmail.com>
2018-02-04 12:14:47 +0530

    add mongo db connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { mongoose } = require('./db/mongoose');
 
 const app = express();
 const port = process.env.PORT || 8000;
