a8f0ad35ad6c27cd7cf2ae86600161a848864b5a
Manish Motiani <manikmanshu@gmail.com>
2018-02-04 12:13:32 +0530

    create nodejs repo

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+function initExpressMiddleWare() {
+    app.use(bodyParser.json());
+}
+
+function initRoutes() {
+    require('./app/routes/router')(app);
+}
+
+function start() {
+    app.listen(port, () => {
+        console.log(`We are live on port ${port}`);
+    })
+}
+
+initExpressMiddleWare();
+initRoutes();
+start();
+
+
+
+
+
+module.exports = { app };
