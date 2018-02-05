a9d66379b8a6635e4d3d1df6cf2ad86097628381
Manish Motiani <manikmanshu@gmail.com>
2018-02-05 09:37:08 +0530

    add pagination component

--- src/public/app/shared/shared.module.ts
+++ src/public/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { PaginationComponent } from "./pagination/pagination.component";
 
 @NgModule({
     imports: [CommonModule, FormsModule],
-    declarations: [],
-    exports: [CommonModule, FormsModule]
+    declarations: [PaginationComponent],
+    exports: [CommonModule, FormsModule, PaginationComponent]
 })
 export class SharedModule { }
