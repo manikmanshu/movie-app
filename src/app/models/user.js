67c9f3d1ef70f50d99863e06d6d9d994e127afe6
manikmanshu <manikmanshu@gmail.com>
2018-03-28 23:52:53 +0530

    add password hash

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
- 
+const bcrypt = require('bcryptjs');
+
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -39,7 +40,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
     user.tokens.push({
         access,
         token
@@ -58,7 +59,7 @@ UserSchema.methods.removeToken = function (token) {
                 token: token
             }
         }
-    }); 
+    });
 };
 
 UserSchema.statics.findByToken = function (token) {
@@ -79,26 +80,38 @@ UserSchema.statics.findByToken = function (token) {
 
 
 UserSchema.statics.findByCredentials = function (email, password) {
-    return this.findOne({email}).
-    then((user) => {
-        if(!user) {
-            return Promise.reject();
-        }
-        return new Promise((resolve, reject) => {
-            bcrypt.compare(password, user.password, (err, res) => {
-                if(res) {
-                    resolve(user);
-                }
-                else {
-                    reject(user);
-                }
+    return this.findOne({ email }).
+        then((user) => {
+            if (!user) {
+                return Promise.reject();
+            }
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if (res) {
+                        resolve(user);
+                    }
+                    else {
+                        reject(user);
+                    }
+                });
             });
-        });
-    })
+        })
 };
 
 
-
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if(user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = { User };
