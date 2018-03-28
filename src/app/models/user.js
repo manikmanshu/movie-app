2b0109d488268028eddc068a3655130cf979a3c3
manikmanshu <manikmanshu@gmail.com>
2018-03-28 12:08:17 +0530

    add user routes

---
+++ src/app/models/user.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+ 
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
+
+UserSchema.methods.toJSON = function () {
+    return _.pick(this.toObject(), ['email', '_id'])
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    user.tokens.push({
+        access,
+        token
+    });
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    }); 
+};
+
+UserSchema.statics.findByToken = function (token) {
+    // methods on User model
+    var decoded;
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return this.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    return this.findOne({email}).
+    then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                }
+                else {
+                    reject(user);
+                }
+            });
+        });
+    })
+};
+
+
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
--- src/app/routes/router.js
+++ src/app/routes/router.js
@@ -1,12 +1,14 @@
 const movieRoutes = require('./movieRoutes');
 const actorRoutes = require('./actorRoutes');
 const directorRoutes = require('./directorRoutes');
+const userRoutes = require('./userRoutes');
 const genre = require('./../../data/genres');
 module.exports = function (app) {    
     // movie api routes
     movieRoutes(app);
     actorRoutes(app);
     directorRoutes(app);
+    userRoutes(app);
 
     app.get('/genres', (req, res) => {
         var genres = Object.keys(genre).map(function (key) { return genre[key]; });
